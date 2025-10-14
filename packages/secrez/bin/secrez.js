6e79ce232e74c837421aa067e9fe316fc19a3d95
Francesco Sullo <sullof@gmail.com>
2025-10-14 09:41:02 +0200

    Change to secrez.js to not block dev executions

--- packages/secrez/bin/secrez.js
+++ packages/secrez/bin/secrez.js
@@ -10,9 +10,9 @@ const pkg = require("../package");
 const MainPrompt = require("../src/prompts/MainPrompt");
 const Logger = require("../src/utils/Logger");
 
-// Check if the package was installed with pnpm
+// Check if the package was installed with pnpm (skip check in development)
 const installPath = __dirname;
-if (installPath.indexOf("pnpm") === -1) {
+if (process.env.NODE_ENV !== "dev" && installPath.indexOf("pnpm") === -1) {
   console.error(chalk.red.bold("\n⚠️  Installation Error\n"));
   console.error(
     chalk.yellow(
